cb822aef3bea397edb035876672234a22b5d18bc
lbischof <1837725+lbischof@users.noreply.github.com>
2019-03-28 16:01:40 +0100

    Move stuff around

--- db/v1/instances/results.go
+++ db/v1/instances/results.go
@@ -3,7 +3,60 @@ package instances
 import (
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/pagination"
+    "strings"
+    "time"
 )
+
+type OTCInstanceList struct {
+	Instances []Instance
+}
+
+type Instance struct {
+	// Indicates the datetime that the instance was created
+	Created time.Time `json:"-"`
+
+	// Indicates the most recent datetime that the instance was updated.
+	Updated time.Time `json:"-"`
+
+	// Indicates the hardware flavor the instance uses.
+	Flavor struct {
+		ID string
+	}
+
+	Hostname string
+
+	// Indicates the unique identifier for the instance resource.
+	ID string
+
+	// The human-readable name of the instance.
+	Name string
+
+	// The build status of the instance.
+	Status string
+
+	// OTC: Indicates the DB instance type, which can be master, slave, or readreplica.
+	Type string
+
+	// OTC: Indicates the region where the DB instance is deployed.
+	Region string
+
+	// OTC: Indicates the AZ ID.
+	AvailabilityZone string
+
+	// OTC: Indicates the VPC ID.
+	VPC string
+
+	// OTC: Indicates the nics information.
+	NICs struct {
+		SubnetID string
+	}
+
+	// OTC: Indicates the security group information.
+	SecurityGroup struct {
+		ID string
+	}
+
+	// OTC: Indicates the database port number.
 
 // InstancePage represents a single page of a paginated instance collection.
 type InstancePage struct {
