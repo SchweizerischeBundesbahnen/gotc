5619b306bca93d26cb9e803bf9456a06376c7c94
lbischof <1837725+lbischof@users.noreply.github.com>
2019-04-04 15:24:57 +0200

    Correctly set scope

--- client.go
+++ client.go
@@ -1,19 +1,19 @@
 package gotc
 
 import (
-    "github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud"
 )
 
 func NewDBV1(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
-    sc, err := initClientOpts(client, eo, "rdsv1")
-    if err != nil {
-        return nil, err
-    }
-    sc.MoreHeaders = map[string]string{
-        "X-Language":   "en-us",
-        "Content-Type": "application/json",
-    }
-    return sc, nil
+	sc, err := initClientOpts(client, eo, "rdsv1")
+	if err != nil {
+		return nil, err
+	}
+	sc.MoreHeaders = map[string]string{
+		"X-Language":   "en-us",
+		"Content-Type": "application/json",
+	}
+	return sc, nil
 }
 
 func initClientOpts(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts, clientType string) (*gophercloud.ServiceClient, error) {
--- identity/v3/agencies/requests.go
+++ identity/v3/agencies/requests.go
@@ -27,9 +27,12 @@ func AuthenticatedClient(opts gophercloud.AuthOptions, tenant, agency string) (*
 }
 
 func getTokenProvider(opts gophercloud.AuthOptions, token string) (*gophercloud.ProviderClient, error) {
+	opts.Scope = new(gophercloud.AuthScope)
+	opts.Scope.ProjectName = opts.TenantName
+	opts.Scope.DomainName = opts.DomainName
 	opts.TokenID = token
 	opts.Username = ""
-    opts.Password = ""
+	opts.Password = ""
 	opts.UserID = ""
 	opts.DomainID = ""
 	opts.DomainName = ""
